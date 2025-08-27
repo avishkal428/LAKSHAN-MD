302a943d7812125e2c97b29b6a47ac8a32797f19
avishkal428 <avishkal907@gmail.com>
2025-08-27 16:49:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "rNMwjLQb#kG90O7JAiGmSbcPf1h3AtPTgIssDZRwAuzbBJVwJEdU",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Manju362/Link-gamu./refs/heads/main/IMG-20250408-WA0003.jpg",
+ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
+};
