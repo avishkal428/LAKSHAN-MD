01cb99f60dfbbfd87958a11b36ce9a4908e1337c
avishkal428 <avishkal907@gmail.com>
2025-09-03 05:02:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SNsBWBLa#Y6yyecDex3CcRz1E0QVcmsApKgLIMqdSvo1aW65QZhM",
+SESSION_ID: process.env.SESSION_ID || "OYF02aTI#aEAuHpnjl94_PZVlXsdM1sxtPXRSeTRuBK7wOb-InXo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/lkvdvv.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 };
