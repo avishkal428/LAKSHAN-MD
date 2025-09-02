44a7f88adccae557086f156de1e1d7031a241c8a
avishkal428 <avishkal907@gmail.com>
2025-09-02 21:37:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "SNsBWBLa#Y6yyecDex3CcRz1E0QVcmsApKgLIMqdSvo1aW65QZhM",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/avishkal428/Lakshan-md-bot/blob/4c46edeef90f8530f3c2cc8088c415388cb648d9/IMG-20250807-WA0075.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/lkvdvv.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 };
