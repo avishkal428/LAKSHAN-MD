6809df34c6b311a4a2da79eb4079690623265f5f
avishkal428 <avishkal907@gmail.com>
2025-09-02 20:51:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "SNsBWBLa#Y6yyecDex3CcRz1E0QVcmsApKgLIMqdSvo1aW65QZhM",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Manju362/Link-gamu./refs/heads/main/IMG-20250408-WA0003.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/avishkal428/Lakshan-md-bot/blob/4c46edeef90f8530f3c2cc8088c415388cb648d9/IMG-20250807-WA0075.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 };
