08d6605489d1138d3b6987567927a510f7e25dda
avishkal428 <avishkal907@gmail.com>
2025-09-05 19:13:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "OYF02aTI#aEAuHpnjl94_PZVlXsdM1sxtPXRSeTRuBK7wOb-InXo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/lkvdvv.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
-MONGODB: process.env.MONGODB || "mongodb+srv://homodik802:G9le4VN9JGn5hcxQ@cluster0.auaczzj.mongodb.net/",
 };
