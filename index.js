098d794b9f14b18b3af29d29e4358756331cda39
𝐋𝐈𝐘𝐀𝐍𝐀𝐀𝐑𝐀𝐂𝐇𝐂𝐇𝐈 𝐀𝐕𝐈𝐒𝐇𝐊𝐀 <avishkal907@gmail.com>
2026-07-18 16:21:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `LAKSHAN-MD-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/lkvdvv.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/uqofdi.jpg` }, caption: up })
 
 }
 })
