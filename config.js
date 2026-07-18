562111f698a194dd7bfc8e4d2a52640aaa6aca40
𝐋𝐈𝐘𝐀𝐍𝐀𝐀𝐑𝐀𝐂𝐇𝐂𝐇𝐈 𝐀𝐕𝐈𝐒𝐇𝐊𝐀 <avishkal907@gmail.com>
2026-07-18 16:03:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "2K52XApQ#ARfPvFWg6dMG8gQs3FXn8fnyzhSGjFfhST1YdyhxnPo",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://biakk-pissek4.hf.space/api/stream/secure/6a38af067006dd1c80422eb7/moviehub%20dl%20moviebot.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/lkvdvv.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 MOVIE_API_KEY: process.env.API_KEY || "sky|2483faa7f5630311464123d017fc7acc2aec6da0",
 };
