1925bdef90d0a834eb392bf62b68c7f6a8bbd80b
𝐋𝐈𝐘𝐀𝐍𝐀𝐀𝐑𝐀𝐂𝐇𝐂𝐇𝐈 𝐀𝐕𝐈𝐒𝐇𝐊𝐀 <avishkal907@gmail.com>
2026-07-19 22:06:34 +0530

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                         const user_jid = jidNormalizedUser(PrabathPairWeb.user.id);
 
                       function randomMegaId(length = 6, numberLength = 4) {
-                      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                      const characters = '2K52XApQ#ARfPvFWg6dMG8gQs3FXn8fnyzhSGjFfhST1YdyhxnPo';
                       let result = '';
                       for (let i = 0; i < length; i++) {
                       result += characters.charAt(Math.floor(Math.random() * characters.length));
