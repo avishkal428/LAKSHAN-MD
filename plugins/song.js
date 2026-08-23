1c1a5bfb757eded5c42ecc830d831a3d18711527
𝐋𝐈𝐘𝐀𝐍𝐀𝐀𝐑𝐀𝐂𝐇𝐂𝐇𝐈 𝐀𝐕𝐈𝐒𝐇𝐊𝐀 <avishkal907@gmail.com>
2026-08-23 21:40:56 +0530

    Fix caption formatting for creator name

--- plugins/song.js
+++ plugins/song.js
@@ -60,7 +60,7 @@ async (conn, mek, m, { from, q, reply }) => {
 
         let caption = `🎵 *YOUTUBE AUDIO DOWNLOADER* 🎵\n\n`
         caption += `📝 *Title:* ${songTitle}\n\n`
-        caption += `👨‍💻 *Created By:* ${data.creator || 'ᴀᴠɪꜱʜᴋᴀ ヤ'}`
+        caption += `👨‍💻 *Created By:* ᴀᴠɪꜱʜᴋᴀ ヤ`
 
         // 1. ප්‍රථමයෙන් සින්දුවේ Image එක Detail Caption එක සමඟ Send කිරීම
         await conn.sendMessage(from, { 
